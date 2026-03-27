13b8861dca1dc6fab2af409b94458fb9e5275a96
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2026-03-27 04:52:56 -0300

    fix misplaced code

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
+import FunctionalInput from './components/FunctionalInput';
+import ClassInput from './components/ClassInput';
+import './style.css';
 
-const FunctionalInput = ({ name }) => {
-  /*
-    We declare two state variables and their setters,
-    one to store the To-Do's
-    and the other to store the value of the input field
-  */
-  const [todos, setTodos] = useState(['Just some demo tasks', 'As an example']);
-  const [inputVal, setInputVal] = useState('');
-
-  const handleInputChange = (e) => {
-    setInputVal(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setTodos((todo) => [...todo, inputVal]);
-    setInputVal('');
-  };
-
+export default function App() {
   return (
-    <section>
-      <h3>{name}</h3>
-      {/* The input field to enter To-Do's */}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="task-entry">Enter a task: </label>
-        <input
-          type="text"
-          name="task-entry"
-          value={inputVal}
-          onChange={handleInputChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <h4>All the tasks!</h4>
-      {/* The list of all the To-Do's, displayed */}
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo}>{todo}</li>
-        ))}
-      </ul>
-    </section>
+    <>
+      <FunctionalInput name="Functional component!" />
+      <div className="divider" />
+      <ClassInput name="Class based component!" />
+    </>
   );
-};
-
-export default FunctionalInput;
+}
 
